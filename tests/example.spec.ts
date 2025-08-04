156ef1532d0559cef1786100254529b3dfa1f7eb
silvo <silvolink@gmail.com>
2025-08-04 16:50:01 +0300

    Added some modifications into "example.spec.ts" file

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -2,23 +2,17 @@ import { test, expect } from '@playwright/test';
 
 test('has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Playwright/);
 });
 
-test('get started link', async ({ page }) => {
+test.only('playwright page content', async ({ page }) => {
   await page.goto('https://playwright.dev/');
-
-  // Check GitHub icon is visible.
   await expect(page.getByRole('link', { name: 'Github repository' })).toBeVisible();
-
-  //Check footer is visible
-  await expect(page.getByRole('contentinfo' )).toBeVisible();
-
-  // Click the get started link.
+  await expect(page.getByText('Playwright enables reliable end-to-end testing for modern web apps.')).toBeVisible();
+  await expect(page.getByRole( 'contentinfo' )).toBeVisible();
+  await expect(page.getByRole( 'banner' )).toBeVisible();
+  await expect(page.getByRole('img', { name: 'Browsers (Chromium, Firefox,' })).toBeVisible();
+  await expect(page.getByText('Any browser • Any platform • One APICross-browser. Playwright supports all')).toBeVisible();
   await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
 });
